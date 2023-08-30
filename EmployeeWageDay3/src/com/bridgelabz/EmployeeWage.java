bcfab42723989ecd9629985627b08c22dde7a3d2
shilpa <shilpabhonde000@gmail.com>
2023-08-30 09:23:27 +0530

    first commit

---
+++ EmployeeWageDay3/src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        int empcheck = 1;
+        double mathval = Math.floor(Math.random()* 10%2);
+        if(empcheck==mathval){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is absent");
+        }
+    }
+}
