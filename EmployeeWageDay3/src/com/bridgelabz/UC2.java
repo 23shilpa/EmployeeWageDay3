07a7ce1e5332c6d06778989f28d5b658ee7e2d4f
shilpa <shilpabhonde000@gmail.com>
2023-08-30 09:26:10 +0530

    UC2 Complete.

---
+++ EmployeeWageDay3/src/com/bridgelabz/UC2.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class UC2 {
+    public static void main(String[] args) {
+        int empcheck=1;
+        int empHr=20;
+        int fulltime =8;
+        double val=Math.floor(Math.random()*10%2);
+        if (empcheck==val){
+            System.out.println("present");
+            empcheck= fulltime * empHr;
+            System.out.println("wage of emp is:"+empcheck);
+        }else{
+            System.out.println("absent");
+        }
+    }
+}
+
